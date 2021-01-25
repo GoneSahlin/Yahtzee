e853a9ca56492e5d1ac9e6f0a12d26739a39db58
GoneSahlin <zsahlin@zagmail.gonzaga.edu>
2021-01-24 17:30:31 -0800

    added rollDie and getValue

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,24 @@
 public class Die {
+
+    static final int DICE_SIDES = 6;
+
+    private int value;
+
+    //Constructor for Die class
+    public Die()
+    {
+        value = 1;
+    }
+
+    //returns value
+    public int getValue()
+    {
+        return value;
+    }
+
+    //generates random number from 1 to DICE_SIDES, and stores in value
+    public void rollDie()
+    {
+        value = (int)(Math.random() * (DICE_SIDES) + 1);
+    }
 }
